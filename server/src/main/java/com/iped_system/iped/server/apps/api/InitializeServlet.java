1f41b215d7ef042c48400990fa3de10b4ed9d393
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-08-08 09:22:04 +0900

    データストアでのデータを使ってログイン処理を行う処理を作成

---
+++ server/src/main/java/com/iped_system/iped/server/apps/api/InitializeServlet.java
@@ -0,0 +1,29 @@
+package com.iped_system.iped.server.apps.api;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by kenji on 2014/08/08.
+ */
+public class InitializeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Entity user = new Entity("User");
+        user.setProperty("lastname", "中垣");
+        user.setProperty("firstname", "健志");
+        user.setProperty("username", "nakaken0629@gmail.com");
+        user.setProperty("password", "password");
+
+        DatastoreService service = DatastoreServiceFactory.getDatastoreService();
+        service.put(user);
+    }
+}
--- server/src/main/java/com/iped_system/iped/server/apps/api/LoginServlet.java
+++ server/src/main/java/com/iped_system/iped/server/apps/api/LoginServlet.java
@@ -1,5 +1,10 @@
 package com.iped_system.iped.server.apps.api;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.iped_system.iped.common.BaseRequest;
 import com.iped_system.iped.common.BaseResponse;
 import com.iped_system.iped.common.LoginRequest;
@@ -16,8 +21,18 @@ public class LoginServlet extends BaseServlet {
         String username = request.getUsername();
         String password = request.getPassword();
 
+        Query.Filter filter = new Query.FilterPredicate("username", Query.FilterOperator.EQUAL, username);
+        Query query = new Query("User").setFilter(filter);
+        DatastoreService service = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery preparedQuery = service.prepare(query);
+        Entity user = preparedQuery.asSingleEntity();
+
         LoginResponse response = new LoginResponse();
-        response.setStatus("password".equals(password) ? "SUCCESS" : "FAIL");
+        if (user != null && password.equals(user.getProperty("password"))) {
+            response.setStatus("SUCCESS");
+        } else {
+            response.setStatus("FAIL");
+        }
         return response;
     }
 }
