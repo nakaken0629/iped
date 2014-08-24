d548ceaa1626729a630d594b7ee1cc210008a3d2
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-08-24 18:28:32 +0900

    キーを変更

--- server/src/main/java/com/iped_system/iped/server/api/servlet/DownloadServlet.java
+++ server/src/main/java/com/iped_system/iped/server/api/servlet/DownloadServlet.java
@@ -19,7 +19,7 @@ public class DownloadServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BlobKey blobKey = new BlobKey(req.getParameter("blob-key"));
+        BlobKey blobKey = new BlobKey(req.getParameter("pictureKey"));
         blobstoreService.serve(blobKey, resp);
     }
 }
