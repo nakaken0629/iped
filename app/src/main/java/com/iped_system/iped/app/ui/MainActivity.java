4aee8076948b381f34f017577b1a739100bd3b86
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-08-24 23:57:15 +0900

    患者とそれ以外で表示されるタブを変更

--- app/src/main/java/com/iped_system/iped/app/ui/MainActivity.java
+++ app/src/main/java/com/iped_system/iped/app/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTabHost;
 import android.widget.TabHost;
 
 import com.iped_system.iped.R;
+import com.iped_system.iped.app.IpedApplication;
 
 public class MainActivity extends FragmentActivity {
 
@@ -17,10 +18,26 @@ public class MainActivity extends FragmentActivity {
         FragmentTabHost host = (FragmentTabHostEx) findViewById(android.R.id.tabhost);
         host.setup(this, getSupportFragmentManager(), R.id.content);
 
+        IpedApplication application = (IpedApplication) getApplication();
+        String role = application.getRole();
+        if ("患者".equals(role)) {
+            createAsPatient(host);
+        } else {
+            createAsMember(host);
+        }
+    }
+
+    private void createAsMember(FragmentTabHost host) {
         TabHost.TabSpec tabSpecMeeting = host.newTabSpec("meeting");
         tabSpecMeeting.setIndicator("ミーティング");
         host.addTab(tabSpecMeeting, MeetingFragment.class, null);
 
+        TabHost.TabSpec tabSpecInterview = host.newTabSpec("interview");
+        tabSpecInterview.setIndicator("インタビュー");
+        host.addTab(tabSpecInterview, InterviewFragment.class, null);
+    }
+
+    private void createAsPatient(FragmentTabHost host) {
         TabHost.TabSpec tabSpecInterview = host.newTabSpec("interview");
         tabSpecInterview.setIndicator("インタビュー");
         host.addTab(tabSpecInterview, InterviewFragment.class, null);
