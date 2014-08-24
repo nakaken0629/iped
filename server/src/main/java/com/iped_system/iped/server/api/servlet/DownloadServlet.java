cb1902d352350faa41fa670d273f205e8bf171c5
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-08-24 17:15:42 +0900

    画像をダウンロードするサーブレットを作成

---
+++ server/src/main/java/com/iped_system/iped/server/api/servlet/DownloadServlet.java
@@ -0,0 +1,25 @@
+package com.iped_system.iped.server.api.servlet;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by kenji on 2014/08/23.
+ */
+public class DownloadServlet extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BlobKey blobKey = new BlobKey(req.getParameter("blob-key"));
+        blobstoreService.serve(blobKey, resp);
+    }
+}
