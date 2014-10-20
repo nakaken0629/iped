f2ab6e3b8ce63800dd69204ed48b4a5a342de62a
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-10-20 22:36:12 +0900

    Pictureクラスのコード共有化

--- app/src/main/java/com/iped_system/iped/app/main/Picture.java
+++ app/src/main/java/com/iped_system/iped/app/main/Picture.java
@@ -10,31 +10,15 @@ import java.io.Serializable;
 /**
 * Created by kenji on 2014/10/11.
 */
-class Picture implements Serializable {
+public class Picture implements Serializable {
     private byte[] original;
     private Bitmap displayBitmap;
     private Bitmap thumbnailBitmap;
 
-    Picture(byte[] original) {
+    public Picture(byte[] original) {
         this.original = original;
-        this.displayBitmap = convertToDisplay(original);
-        this.thumbnailBitmap = convertToThumbnail(original);
-    }
-
-    Bitmap getDisplayBitmap() {
-        return this.displayBitmap;
-    }
-
-    Bitmap getThumbnailBitmap() {
-        return this.thumbnailBitmap;
-    }
-
-    private Bitmap convertToDisplay(byte[] original) {
-        return convertTo(original, 800);
-    }
-
-    private Bitmap convertToThumbnail(byte[] original) {
-        return convertTo(original, 200);
+        this.displayBitmap = convertTo(original, 800);
+        this.thumbnailBitmap = convertTo(original, 200);
     }
 
     private Bitmap convertTo(byte[] original, int size) {
@@ -55,20 +39,11 @@ class Picture implements Serializable {
         return Bitmap.createBitmap(workBitmap, 0, 0, workBitmap.getWidth(), workBitmap.getHeight(), null, false);
     }
 
-    private byte[] getDisplay() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try {
-            this.displayBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-            baos.flush();
-        } catch (IOException e) {
-            /* nop */
-        } finally {
-            try {
-                baos.close();
-            } catch (IOException e) {
-                /* nop */
-            }
-        }
-        return baos.toByteArray();
+    public Bitmap getDisplayBitmap() {
+        return this.displayBitmap;
+    }
+
+    public Bitmap getThumbnailBitmap() {
+        return this.thumbnailBitmap;
     }
 }
