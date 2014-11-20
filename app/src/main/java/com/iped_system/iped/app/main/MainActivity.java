8eb8cd00d62c05cbd8fe3e143a9f62d3ecf0756f
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-11-20 23:46:22 +0900

    アクションバーを準備

--- app/src/main/java/com/iped_system/iped/app/main/MainActivity.java
+++ app/src/main/java/com/iped_system/iped/app/main/MainActivity.java
@@ -1,19 +1,21 @@
 package com.iped_system.iped.app.main;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.app.ActionBar;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.util.LruCache;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
 import android.widget.TabHost;
 
 import com.iped_system.iped.R;
 import com.iped_system.iped.app.IpedApplication;
 import com.iped_system.iped.common.RoleType;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.OnNavigationListener {
     private LruCache<String, Bitmap> imageCache;
 
     @Override
@@ -29,42 +31,46 @@ public class MainActivity extends FragmentActivity {
             }
         };
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+        adapter.add("中垣健志");
+        adapter.add("永谷真一郎");
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        actionBar.setListNavigationCallbacks(adapter, this);
+
         setContentView(R.layout.activity_main);
         FragmentTabHost host = (FragmentTabHostEx) findViewById(android.R.id.tabhost);
         host.setup(this, getSupportFragmentManager(), R.id.content);
 
         IpedApplication application = (IpedApplication) getApplication();
         RoleType role = application.getRole();
-        if (role == RoleType.PATIENT) {
-            createAsPatient(host);
-        } else {
-            createAsMember(host);
+        if (role != RoleType.PATIENT) {
+            prepareMeetingTab(host);
         }
+        prepareInterviewTab(host);
     }
 
-    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-        if (getBitmapFromMemCache(key) == null) {
-            this.imageCache.put(key, bitmap);
-        }
-    }
-
-    public Bitmap getBitmapFromMemCache(String key) {
-        return this.imageCache.get(key);
-    }
-
-    private void createAsMember(FragmentTabHost host) {
+    private void prepareMeetingTab(FragmentTabHost host) {
         TabHost.TabSpec tabSpecMeeting = host.newTabSpec("meeting");
         tabSpecMeeting.setIndicator("ミーティング");
         host.addTab(tabSpecMeeting, MeetingFragment.class, null);
+    }
 
+    private void prepareInterviewTab(FragmentTabHost host) {
         TabHost.TabSpec tabSpecInterview = host.newTabSpec("interview");
         tabSpecInterview.setIndicator("インタビュー");
         host.addTab(tabSpecInterview, InterviewFragment.class, null);
     }
 
-    private void createAsPatient(FragmentTabHost host) {
-        TabHost.TabSpec tabSpecInterview = host.newTabSpec("interview");
-        tabSpecInterview.setIndicator("インタビュー");
-        host.addTab(tabSpecInterview, InterviewFragment.class, null);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        return true;
     }
 }
