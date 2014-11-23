08278d619817727f4518f9e7b05cb96345409aaf
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-11-23 14:06:14 +0900

    close #23 システム系のピクトグラムを削除。ID名を変更

--- app/src/main/java/com/iped_system/iped/app/main/PictogramFragment.java
+++ app/src/main/java/com/iped_system/iped/app/main/PictogramFragment.java
@@ -9,12 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.ImageView;
 
 import com.iped_system.iped.R;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 
 public class PictogramFragment extends DialogFragment {
     private static final String TAG = PictogramFragment.class.getName();
@@ -43,23 +39,18 @@ public class PictogramFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_pictogram, container, false);
-        rootView.findViewById(R.id.imageView1).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView2).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView3).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView4).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView5).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView6).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView7).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView8).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView9).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView10).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView11).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView12).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView101).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView102).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView102b).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView103).setOnClickListener(new PictogramListener());
-        rootView.findViewById(R.id.imageView104).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon001ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon002ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon003ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon004ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon005ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon006ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon007ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon008ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon009ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon010ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon011ImageView).setOnClickListener(new PictogramListener());
+        rootView.findViewById(R.id.icon012ImageView).setOnClickListener(new PictogramListener());
         return rootView;
     }
 
