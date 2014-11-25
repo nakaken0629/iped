d22316bb6fc1da5c182d6587a2cd57c5466a4b5d
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-11-26 00:09:57 +0900

    refs #32 リリース準備

--- server/src/main/java/com/iped_system/iped/server/api/servlet/VersionServlet.java
+++ server/src/main/java/com/iped_system/iped/server/api/servlet/VersionServlet.java
@@ -14,8 +14,8 @@ public class VersionServlet extends BaseServlet {
     @Override
     protected BaseResponse execute(BaseRequest baseRequest) {
         VersionResponse response = new VersionResponse();
-        response.setVersionCode(4);
-        response.setUrl("https://raw.github.com/wiki/nakaken0629/iped/apk/app-server-debug-1.0.0.apk");
+        response.setVersionCode(5);
+        response.setUrl("https://raw.github.com/wiki/nakaken0629/iped/apk/app-server-debug-1.0.apk");
         return response;
     }
 }
