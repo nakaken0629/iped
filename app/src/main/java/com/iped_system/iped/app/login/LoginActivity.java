7fa98b96be998c39ad267f8a0ce658f0cb7ab5af
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-07-19 22:25:52 +0900

    refs #42 GCMのサンプルを組み込んだ

--- app/src/main/java/com/iped_system/iped/app/login/LoginActivity.java
+++ app/src/main/java/com/iped_system/iped/app/login/LoginActivity.java
@@ -1,16 +1,33 @@
 package com.iped_system.iped.app.login;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.iped_system.iped.R;
 import com.iped_system.iped.app.IpedApplication;
+import com.iped_system.iped.app.common.service.QuickstartPreferences;
+import com.iped_system.iped.app.common.service.RegistrationIntentService;
 import com.iped_system.iped.app.main.MainActivity;
 
 public class LoginActivity extends FragmentActivity implements LoginFragment.OnLoginListener {
+    private static final String TAG = LoginActivity.class.getName();
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
     public static final String FRAGMENT_LOGOUT = "LOGOUT";
     public static final String KEY_LOGOUT = "LOGOUT";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+//    private ProgressBar mRegistrationProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,45 @@ public class LoginActivity extends FragmentActivity implements LoginFragment.OnL
                     .add(R.id.container, new LoginFragment(), FRAGMENT_LOGOUT)
                     .commit();
         }
+
+//        mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+//                mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    Log.d(TAG, getString(R.string.gcm_send_message));
+//                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+                } else {
+                    Log.d(TAG, getString(R.string.token_error_message));
+//                    mInformationTextView.setText(getString(R.string.token_error_message));
+                }
+            }
+        };
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Log.d(TAG, "start RegistrationIntentService");
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 
     @Override
@@ -40,5 +96,20 @@ public class LoginActivity extends FragmentActivity implements LoginFragment.OnL
             LoginFragment fragment = (LoginFragment) getSupportFragmentManager().findFragmentByTag(FRAGMENT_LOGOUT);
             fragment.resetTextView();
         }
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
